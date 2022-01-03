2b4ac46460b38df982464aeed92c6dbaa041afdd
jerin <jerinmanjadiyil@gmail.com>
2022-01-03 22:53:24 +0530

    helpers.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React,{useState} from 'react'
-import Board from './components/Board'
-import { calculateWinner } from './helpers';
+import React, { useState } from 'react';
+import Board from './components/Board';
+import { calculateWinner } from './helpers.jsx';
+
 import './styles/root.scss';
+
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
+
   const winner = calculateWinner(board);
   const message = winner
     ? `Winner is ${winner}`
@@ -27,12 +30,14 @@ const App = () => {
 
     setIsXNext(prev => !prev);
   };
-  return(
-  <div className='app'>
-    <h1>Tic Tac Toe</h1>
-    <h2>{message}</h2>
-    <Board board={board} handleSquareClick={handleSquareClick}/>
-  </div>
-  )
-}
+
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
+    </div>
+  );
+};
+
 export default App;
